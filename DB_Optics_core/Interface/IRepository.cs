fefd1370d32009493934269958f88b1cf9716c0d
Смутин Даниил <Dsmutin2003@yandex.ru>
2022-06-16 20:59:41 +0300

    Добавил репозиторий

---
+++ DB_Optics_core/Interface/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB_Optics_core.Interface
+{
+    public interface IRepository<T>
+    {
+        T[] getAll();
+        T getByID(int ID);
+        T add(T entity);
+        T update(T entity);
+        void delete(T entity);
+    }
+}
