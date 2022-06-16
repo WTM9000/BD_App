23cbabad4212fd8b7e68730de6f014b51e686d05
Смутин Даниил <Dsmutin2003@yandex.ru>
2022-06-16 17:24:19 +0300

    Добавьте файлы проекта.

---
+++ BD_App_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BD_App_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string connectionString;
+
+            try
+            {
+                connectionString = "server=127.0.0.1;uid=root;pwd=M4SOPMODIIJr;database=optics";
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = connectionString;
+                conn.Open();
+                Console.WriteLine("Успешно");
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
