caa53d39d23ed0e6b95e25f9d8a113f75054752d
Смутин Даниил <Dsmutin2003@yandex.ru>
2022-06-16 18:36:31 +0300

    Проверка подключения БД

--- BD_App_2/Program.cs
+++ BD_App_2/Program.cs
@@ -13,9 +13,27 @@ namespace BD_App_2
             {
                 connectionString = "server=127.0.0.1;uid=root;pwd=M4SOPMODIIJr;database=optics";
                 conn = new MySql.Data.MySqlClient.MySqlConnection();
+
                 conn.ConnectionString = connectionString;
+
                 conn.Open();
-                Console.WriteLine("Успешно");
+
+                var command = conn.CreateCommand();
+
+                command.CommandText = "select count(*) from `discount card` where `Expiry date` > curdate() and `Register date` < curdate();";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetInt32(0)}");
+                }
+
+                reader.Close();
+
+                command.Dispose();
+
+                conn.Close();
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
